1c667c41c26aa8b945f38777467be2acec2088c9
Aleksandr Latyntsev <latyntsev.a@gmail.com>
2020-07-31 16:20:51 +0300

    update package file

--- Package.swift
+++ Package.swift
@@ -3,5 +3,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftWebSocket"
+    name: "SwiftWebSocket",
+    platforms: [.iOS(.v8), .macOS(.v10_10),.tvOS(.v9)],
+    products: [
+        .library(name: "SwiftWebSocket", targets: ["SwiftWebSocket"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "SwiftWebSocket", dependencies: [], path: "Source")
+    ]
 )
